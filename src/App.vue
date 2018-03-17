eca37ee1ec98184a24cfb7142099bb6bd4caee26
Konstantin <kostya@Konstantin-Debian>
2018-03-17 16:12:44 +0300

    added content app

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template lang="html">
+    <main>
+
+    </main>
+</template>
+
+<script>
+
+export default {
+    data: () => ({
+    }),
+    methods: {
+    },
+    components: {
+    }
+}
+</script>
+
+<style lang="scss">
+main {
+    max-width: 1200px;
+    width: 100%;
+    margin: auto;
+    margin-top: 40px;
+    flex-grow: 1;
+    box-sizing: border-box;
+    border: 1px solid #888;
+    border-top-width: 0;
+    border-bottom-width: 0;
+}
+</style>
